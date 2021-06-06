c4be14a14a4b5726ee9486a336dc89572e8b4a4d
Asim Razzaq <asimrazzaq46@hotmail.com>
2021-06-06 19:10:33 +0200

    change

--- client/src/reducers/AuthUsers/AuthReducer.js
+++ client/src/reducers/AuthUsers/AuthReducer.js
@@ -2,16 +2,21 @@ import {
   LOGIN_REQUEST,
   LOGIN_FAIL,
   LOGIN_SUCCESS,
+  LOAD_USER_REQUEST,
+  LOAD_USER_SUCCESS,
+  LOAD_USER_FAIL,
   CLEAR_ERRORS,
 } from "../../constants/userConstants";
 const authReducer = (state = { user: {} }, action) => {
   switch (action.type) {
     case LOGIN_REQUEST:
+    case LOAD_USER_REQUEST:
       return {
         loading: true,
         isAuthenticated: false,
       };
     case LOGIN_SUCCESS:
+    case LOAD_USER_SUCCESS:
       return {
         ...state,
         loading: false,
@@ -21,6 +26,13 @@ const authReducer = (state = { user: {} }, action) => {
     case LOGIN_FAIL:
       return {
         ...state,
+        loading: false,
+        isAuthenticated: false,
+        user: null,
+        error: action.payload,
+      };
+    case LOAD_USER_FAIL:
+      return {
         loading: false,
         isAuthenticated: false,
         user: null,
